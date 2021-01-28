cf98fefe2bf5dc16d43ff04baf5f19664608114c
Chao Zhang <raidenchao@gmail.com>
2021-01-27 18:58:13 -0800

    first commit

---
+++ class4/script.js
@@ -0,0 +1,15 @@
+// input: prompt()
+// output: alert()
+
+function abc() {
+  let lastname = prompt('What is your last name ?')
+  let firstname = prompt('What is your first name ?')
+  let name = firstname + ' ' + lastname
+  alert('Your full name is: '  + name)
+} 
+
+function change() {
+  document.getElementById('title').innerText = 'Python'
+}
+
+
